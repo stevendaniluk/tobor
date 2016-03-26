4d7cda384c43621d666e6d821b8daa37c182a596
Steven Daniluk <stevendaniluk8@gmail.com>
2016-03-25 21:55:02 -0400

    Add docking to send_goal, and create go_and_return script

--- src/go_and_return.cpp
+++ src/go_and_return.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv){
   
   // Check input arguments
   if (argc != 4 && argc!=5) {
-    ROS_INFO("Usage: Input arguments [x, y, theta] or [x, y, theta, dock]");
+    ROS_INFO("Usage: Input arguments [x, y, theta] or [x, y, theta, frame_id]");
     return 1;
   }// end if
   
@@ -24,22 +24,26 @@ int main(int argc, char** argv){
   
   // Assign input arguments to goal object
   Tobor.setCurrentGoal(atof(argv[1]), atof(argv[2]), atof(argv[3]));
+  if (argc == 5) {
+    Tobor.current_goal.target_pose.header.frame_id = argv[4];
+  }// end if
   
   // Send the goal
   Tobor.sendGoal(Tobor.current_goal);
   
   ros::Duration(1.0).sleep();
   
-  // If successful, start docking
+  // If successful, return to start
   if (Tobor.nav_state == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    // If docking requested, begin
-    if (argc == 5 && (std::strcmp(argv[4], "dock") || std::strcmp(argv[4], "Dock"))) {
-      ROS_INFO("Beginning docking.");
-      Tobor.dock();
-    }// end if
+    ROS_INFO("Returning to start position.");
+    Tobor.setCurrentGoal(Tobor.start_pose.pose.pose.position.x, 
+                         Tobor.start_pose.pose.pose.position.y, 
+                         Tobor.start_pose.pose.pose.orientation.z);
+    Tobor.sendGoal(Tobor.current_goal);
   }else {
-    ROS_INFO("Did not succeed, will not dock.");
+    ROS_INFO("Did not Succeed.");
   }// end if (goal)
+  
 
   return 0;
 }
